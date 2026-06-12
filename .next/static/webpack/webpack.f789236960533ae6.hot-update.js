818f252ead0cd5ec6cd6b93929e06b007ed72830
rifath23 <rifath0422@gmail.com>
2026-06-12 15:38:40 +0600

    blog updated- how-to-reduce-screen-time-as-a-student with sitemap

--- .next/static/webpack/webpack.f789236960533ae6.hot-update.js
+++ .next/static/webpack/webpack.f789236960533ae6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2e0e627f6ac0ed01"; }
+/******/ 	__webpack_require__.h = function() { return "bed8703123223645"; }
 /******/ }();
 /******/ 
 /******/ }
