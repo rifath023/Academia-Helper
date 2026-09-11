69f76643bc3ae9065d69d5da0c01d2841acd5bf3
rifath23 <rifath0422@gmail.com>
2026-09-11 11:53:01 +0600

    trail slash problem solution

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,26 +1,54 @@
 import fs from 'fs';
 import path from 'path';
+import Head from 'next/head';
 import type { GetStaticProps, GetStaticPaths } from 'next';
 import postsData from '../../blog-posts/index.json';
 
 interface Props {
   htmlContent: string;
+  slug: string;
 }
 
-export default function BlogPost({ htmlContent }: Props) {
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+export default function BlogPost({ htmlContent, slug }: Props) {
+  const canonicalUrl = `https://www.academiahelper.com/blog/${slug}/`;
+
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
+
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    </>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = postsData.map((post) => ({
     params: { slug: post.slug },
   }));
-  return { paths, fallback: false };
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slug = params?.slug as string;
-  const filePath = path.join(process.cwd(), 'blog-posts', `${slug}.html`);
+
+  const filePath = path.join(
+    process.cwd(),
+    'blog-posts',
+    `${slug}.html`
+  );
+
   const htmlContent = fs.readFileSync(filePath, 'utf-8');
-  return { props: { htmlContent } };
+
+  return {
+    props: {
+      htmlContent,
+      slug,
+    },
+  };
 };
