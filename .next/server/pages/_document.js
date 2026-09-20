8375d7bec169dda5ae282a67db3763a41cbb0dbe
rifath23 <rifath0422@gmail.com>
2026-09-20 16:13:13 +0600

    new blog posts 5

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,11 @@
+"use strict";(()=>{var e={};e.id=660,e.ids=[660],e.modules={7645:(e,t,i)=>{i.r(t),i.d(t,{default:()=>s});var r=i(997),n=i(6859);function s(){return(0,r.jsxs)(n.Html,{lang:"en",children:[(0,r.jsxs)(n.Head,{children:[r.jsx("link",{rel:"icon",type:"image/png",href:"/logo.png"}),r.jsx("meta",{name:"robots",content:"index, follow"}),r.jsx("meta",{property:"og:site_name",content:"Academia Helper"}),r.jsx("meta",{property:"og:type",content:"website"}),r.jsx("script",{src:"https://identity.netlify.com/v1/netlify-identity-widget.js"})]}),(0,r.jsxs)("body",{children:[r.jsx(n.Main,{}),r.jsx(n.NextScript,{}),r.jsx("script",{dangerouslySetInnerHTML:{__html:`
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                  });
+                }
+              });
+            }
+          `}})]})]})}},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},997:e=>{e.exports=require("react/jsx-runtime")},5315:e=>{e.exports=require("path")}};var t=require("../webpack-runtime.js");t.C(e);var i=e=>t(t.s=e),r=t.X(0,[859],()=>i(7645));module.exports=r})();
